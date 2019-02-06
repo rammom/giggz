f1be8625680589328bcb99b72d644b8165869022
moose <mr.rammo@gmail.com>
2019-02-06 05:15:18 -0500

    add frontend react server

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const indexController = require('../controllers/index_controller');
 
 /* GET home page. */
-router.get('/', indexController);
+router.get('/', indexController.home);
 
 module.exports = router;
