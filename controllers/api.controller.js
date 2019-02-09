d5aaf9719a2c911d55bbc5e42223ad0f91d6a526
AbdulAbuLibda <Abdul@MacBook-Air-2.local>
2019-02-09 02:02:56 -0500

    fixing addEmployee API

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -17,47 +17,7 @@ const index = {
 	}
 }
 
-const employee = {
-	/*
-	*		Edit an existing employee
-	*/
-	create: async (req, res, next) => {
-		const id = req.body.id;
 
-		const name = req.body.name;
-		const hours = req.body.hours;
-		const store = req.body.store;
-		const services = req.body.services;
-		const role = req.body.role;
-		
-
-		// Verify non-empty data
-		if (!name || !hours || !store || !services || !role){
-			return handleError(res, null, 400, `ERROR: name, address and hours are required!`);
-		}
-
-		//Verification of Hours data before creating Availability Object
-		if (!verify.checkObjectProperties(hours, ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])){
-			return res.status(400).json({
-				error: `ERROR: hours not formatted properly!`
-			});
-		}
-
-		//Creating the Availability Object
-		let availability = new Availability(hours);
-
-		await availability.save()
-			.catch((err) => {
-				return res.status(500).json({
-					error: `ERROR saving availability failed: ${err}`
-				});
-			});
-		res.status(200).json({
-			msg: `Store created!`
-		});
-
-	}
-}
 
 
 /*
@@ -117,11 +77,7 @@ const store = {
 		const email = req.body.email.toUpperCase();
 		const storeid = req.body.storeid;
 
-		// check for duplicates in db
-		let duplicate_fields = [];
-		let find_error = null;
-		
-		
+		let find_error = null;		
 		let user = null; //Checking if user exists
 		await User.findOne({email})
 			.then( u => user = u )
@@ -129,39 +85,47 @@ const store = {
 		if (!user){
 			return sendResponse(res, "User not found");
 		}
-		console.log(user.employee);
 		if (user.employee){ //Makes sure user isn't already employed
 			return sendResponse(res, "Failed to add employee!");
 		}
 
 		let store = null; //Checking if store exists
 		await Store.findOne({_id:storeid})
-		.then( s => store = s )
-		.catch( err => find_error = err );
+			.then( s => store = s )
+			.catch( err => find_error = err );
 		if (!store){
 			return sendResponse(res, "Store not found");
 		}
 
 		let save_error = null; //Creating Employee
-		let new_Employee = new Employee(
+		let new_employee = new Employee(
 			{
-				user:user,
-				store:store,
+				user:user._id,
+				store:store._id,
 				role:"employee"
 			}
 		);
 
-		await new_Employee.save() //Making sure employee saves properly
+		await new_employee.save() //Making sure employee saves properly
 			.catch((err) => save_error = err);
 		if(save_error){
 			return handleError(res, save_error, 500, "while saving employee");
 		}
 
-		user.employee = new_Employee; //Changing user status to reflect employment
+		user.employee = new_employee._id; //Changing user status to reflect employment
 		await user.save()
 			.catch((err) => save_error = err);
 		if (save_error){ 
-			new_Employee.remove(); //Remove employee if user employment can't be changed
+			new_employee.remove(); //Remove employee if user employment can't be changed
+			return handleError(res, save_error, 500, "while saving employee");
+		}
+
+		store.employees.push(new_employee._id); //Changing store employee directory
+		await store.save()
+			.catch((err) => save_error = err);
+		if (save_error){ 
+			new_employee.remove(); //Remove employee if store employees can't be changed
+			return handleError(res, save_error, 500, "while saving employee");
 		}
 
 		return sendResponse(res, "Employee successfully created");
