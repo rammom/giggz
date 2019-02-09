ca64c79903afe3dbf6c617057c6789ad8d12734c
AbdulAbuLibda <Abdul@MacBook-Air-2.local>
2019-02-08 22:23:10 -0500

    Adding employee functions

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,4 +1,6 @@
 const Store = require('../models/Store');
+const Employee = require('../models/Employee');
+const Service = require('../models/Service');
 const Availability = require('../models/Availability');
 const ttm = require('../utils/utils').time_to_minutes;
 const handleError = require('../utils/utils').handleError;
@@ -14,9 +16,55 @@ const index = {
 	}
 }
 
+<<<<<<< HEAD
 /*
 	store functions
 */
+=======
+const employee = {
+	/*
+	*		Create a new employee
+	*/
+	create: async (req, res, next) => {
+
+		const name = req.body.name;
+		const hours = req.body.hours;
+		const store = req.body.store;
+		const services = req.body.services;
+		const role = req.body.role;
+		
+
+		// Verify non-empty data
+		if (!name || !hours || !store || !services || !role){
+			return res.status(400).json({
+				error: `ERROR: name, address and hours are required!`
+			});
+		}
+
+		//Verification of Hours data before creating Availability Object
+		if (!verify.checkObjectProperties(hours, ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])){
+			return res.status(400).json({
+				error: `ERROR: hours not formatted properly!`
+			});
+		}
+
+		//Creating the Availability Object
+		let availability = new Availability(hours);
+
+		await availability.save()
+			.catch((err) => {
+				return res.status(500).json({
+					error: `ERROR saving availability failed: ${err}`
+				});
+			});
+		res.status(200).json({
+			msg: `Store created!`
+		});
+
+	}
+}
+
+>>>>>>> Adding employee functions
 const store = {
 
 	/**
