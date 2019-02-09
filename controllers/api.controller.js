867bdc25f95255b0799ac2d05ee22b8282371f02
AbdulAbuLibda <Abdul@MacBook-Air-2.local>
2019-02-09 01:24:41 -0500

    AddEmployee API

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,6 +1,7 @@
 const Store = require('../models/Store');
 const Employee = require('../models/Employee');
 const Service = require('../models/Service');
+const User = require('../models/User');
 const Availability = require('../models/Availability');
 const ttm = require('../utils/utils').time_to_minutes;
 const handleError = require('../utils/utils').handleError;
@@ -18,9 +19,10 @@ const index = {
 
 const employee = {
 	/*
-	*		Create a new employee
+	*		Edit an existing employee
 	*/
 	create: async (req, res, next) => {
+		const id = req.body.id;
 
 		const name = req.body.name;
 		const hours = req.body.hours;
@@ -31,9 +33,7 @@ const employee = {
 
 		// Verify non-empty data
 		if (!name || !hours || !store || !services || !role){
-			return res.status(400).json({
-				error: `ERROR: name, address and hours are required!`
-			});
+			return handleError(res, null, 400, `ERROR: name, address and hours are required!`);
 		}
 
 		//Verification of Hours data before creating Availability Object
@@ -111,8 +111,62 @@ const store = {
 
 		return sendResponse(res, {store}, "store created");
 
-	}
+	},
 
+	addEmployee: async (req, res, next) =>{
+		const email = req.body.email.toUpperCase();
+		const storeid = req.body.storeid;
+
+		// check for duplicates in db
+		let duplicate_fields = [];
+		let find_error = null;
+		
+		
+		let user = null; //Checking if user exists
+		await User.findOne({email})
+			.then( u => user = u )
+			.catch( err => find_error = err );
+		if (!user){
+			return sendResponse(res, "User not found");
+		}
+		console.log(user.employee);
+		if (user.employee){ //Makes sure user isn't already employed
+			return sendResponse(res, "Failed to add employee!");
+		}
+
+		let store = null; //Checking if store exists
+		await Store.findOne({_id:storeid})
+		.then( s => store = s )
+		.catch( err => find_error = err );
+		if (!store){
+			return sendResponse(res, "Store not found");
+		}
+
+		let save_error = null; //Creating Employee
+		let new_Employee = new Employee(
+			{
+				user:user,
+				store:store,
+				role:"employee"
+			}
+		);
+
+		await new_Employee.save() //Making sure employee saves properly
+			.catch((err) => save_error = err);
+		if(save_error){
+			return handleError(res, save_error, 500, "while saving employee");
+		}
+
+		user.employee = new_Employee; //Changing user status to reflect employment
+		await user.save()
+			.catch((err) => save_error = err);
+		if (save_error){ 
+			new_Employee.remove(); //Remove employee if user employment can't be changed
+		}
+
+		return sendResponse(res, "Employee successfully created");
+	}
+		
 
 }
 
--- routes/api.store.route.js
+++ routes/api.store.route.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const apiController = require('../controllers/api.controller');
 
 router.post('/', apiController.store.create);
-
+router.post('/addemployee', apiController.store.addEmployee);
 
 module.exports = router;
