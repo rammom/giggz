d14ab9c6ae589af38524ff31d2fa5411b2bf1829
moose <mr.rammo@gmail.com>
2019-02-08 23:16:27 -0500

    change to store test, still incomplete

--- test/store.test.js
+++ test/store.test.js
@@ -6,30 +6,32 @@ const request = require('supertest')(url);
 
 const ttm = require('../utils/utils').time_to_minutes;
 
-describe('Store', function() {
+describe('Store', () => {
 
 	context('create', () => {
 		it('with proper arguments', function(done) {
 			
+			let store_data = {
+				name: "Giggs Cuts",
+				address: {
+					street: "2110 Wyandotte St",
+					city: "Windsor",
+					state: "Ontario",
+					country: "Canada"
+				},
+				hours: {
+					monday: [{ start: ttm(9), end: ttm(17) }],
+					tuesday: [{ start: ttm(9), end: ttm(17) }],
+					wednesday: [{ start: ttm(9), end: ttm(17) }],
+					thursday: [{ start: ttm(9), end: ttm(17) }],
+					friday: [{ start: ttm(9), end: ttm(17) }],
+					saturday: [{ start: ttm(9), end: ttm(17) }],
+					sunday: [{ start: ttm(9), end: ttm(17) }],
+				}
+			}
+
 			request.post('/api/store')
-				.send({
-					name: "Giggs Cuts",
-					address: {
-						street: "2110 Wyandotte St",
-						city: "Windsor",
-						state: "Ontario",
-						country: "Canada"
-					},
-					hours: {
-						monday:		[{ start: ttm(9), end: ttm(17) }],
-						tuesday:	[{ start: ttm(9), end: ttm(17) }],
-						wednesday:	[{ start: ttm(9), end: ttm(17) }],
-						thursday:	[{ start: ttm(9), end: ttm(17) }],
-						friday:		[{ start: ttm(9), end: ttm(17) }],
-						saturday:	[{ start: ttm(9), end: ttm(17) }],
-						sunday:		[{ start: ttm(9), end: ttm(17) }],
-					}
-				})
+				.send(store_data)
 				.expect(200)
 				.end(function(err, res){
 					if (err) return done(err);
