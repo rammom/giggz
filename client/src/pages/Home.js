a817d5f98e212d4381c926fe38206e92ee607812
moose <mr.rammo@gmail.com>
2019-02-16 16:01:05 -0500

    in progress

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+	return (
+	  <React.Fragment>
+		Hello
+	  </React.Fragment>
+	)
+  }
+}
+
+export default Home
---
+++ client/src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PageNotFound extends Component {
+  render() {
+	return (
+	  <React.Fragment>
+		404 page not found
+	  </React.Fragment>
+	)
+  }
+}
+
+export default PageNotFound
