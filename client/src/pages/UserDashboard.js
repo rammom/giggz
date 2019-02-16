69404121220694dae1a262a914a6444779256685
moose <mr.rammo@gmail.com>
2019-02-16 16:01:05 -0500

    in progress

---
+++ client/src/pages/UserDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class UserDashboard extends Component {
+  render() {
+	return (
+	  <div>
+		User Dashboard
+	  </div>
+	)
+  }
+}
+
+export default UserDashboard;
