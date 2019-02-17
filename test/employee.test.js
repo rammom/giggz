25cf01ea55fe23ef631e65b5e1a1c6aeff4e000b
AbdulAbuLibda <Abdul@MacBook-Air-2.local>
2019-02-16 20:25:34 -0500

    employee registration testing

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,5 +1,4 @@
 const assert = require('chai').assert;
-
 const config = require('../config.json').development;
 const url = `${config.protocol}://${config.address}:${config.port}`;
 const request = require('supertest')(url);
@@ -12,11 +11,21 @@ const ttm = require('../utils/utils').time_to_minutes;
 const mochaAsync = require('../utils/utils').mochaAsync;
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Giggs');
+mongoose.connect('mongodb://localhost:27017/Giggs', {useNewUrlParser:true});
+
+
+
+let USER, STORE, EMPLOYEE, AVAILABILITY;
+
 
 
 describe('Employee', () => {
-
+    after(async () => {
+        await USER.remove();
+        await EMPLOYEE.remove();
+        await STORE.remove();
+        await Availability.findByIdAndDelete(STORE.hours);
+    })
     const gen_user_data = () => ({
         email: "employeetest@giggs.ca",
         password: "passw0rd._!?",
@@ -59,12 +68,22 @@ describe('Employee', () => {
     });
     
     
-	context('Availability', () => {
+	context('Registration', () => {
         const register_route = '/auth/register';
         const store_route = '/api/store';
         const employee_route = '/api/store/employee';
 
-		it('Update with proper arguments', async function() {
+		it('Creating an employee', async function() {
+            /*
+            A pretty thorough test: 
+            It adds a brand new user to database using API
+            Makes sure that user is created properly, then object is pulled up
+            A cookie is created and used for authentication
+            Creates a store using API and verifies its made properly
+            Pulls up store from database
+            Creates Employee using Store and User objects
+            Pulls up employee from database
+            */
             let registration_data = gen_user_data();
 			await request.post(register_route)
 				.send(registration_data)
@@ -77,11 +96,6 @@ describe('Employee', () => {
 
 
             let user = null; //Finding user
-            //Auth.findOne({nick: 'noname'}, function(err,obj) { console.log(obj); });
-
-            //User.findOne({email:registration_data.email.toUpperCase()},function(err,obj){console.log(obj)});
-
-            
             let find_error = null;
             await User.findOne({email:registration_data.email.toUpperCase()})
                 .then(u => {
@@ -95,7 +109,6 @@ describe('Employee', () => {
 
 
             let cookie;
-            console.log(user);
             await request.post('/auth/login')
                 .send({ email: registration_data.email, password: registration_data.password })
                 .expect(200)
@@ -105,8 +118,6 @@ describe('Employee', () => {
                 .catch((err) => {
                     done(err);
                 });
-            console.log(`Cookie: ${cookie}`);
-            console.log('yolo');
 
 
             let store_data = gen_store_data();
@@ -128,14 +139,11 @@ describe('Employee', () => {
             assert(store != null, `Store not created!`);
             if (find_error)
                 return done(find_error);
-    
-    
+
             let employee_data = {
                 storeid: store._id,
                 email: registration_data.email
             };
-
-            console.log(cookie);
             await request.post(employee_route)
                 .set('Cookie', cookie)
                 .send(employee_data)
@@ -145,42 +153,26 @@ describe('Employee', () => {
                 .catch((err) => {
                     done(err);
                 });
-            
-            
-            
-            
             let employee = null; //Finding employee
-			await Employee.findOne({user:user._id}, (e,u) => {
-                employee = u;
-                console.log('finished..');
-                console.log(u);
+			await Employee.findOne({user:user._id}, (error,e) => {
+                employee = e;
             })
-
-            // console.log("user1",user);
-            await user.remove()
-                .then();
-
-            mongoose.connection.close();
-
-            // console.log("user2",user);
-            // console.log('emp');
-            // console.log(employee);
-            //done();
-            // assert.then(done());
-
-            // if(!employee){
-            //     console.log('fuck this shit');
-            // }
-
-            // assert(true);
-			// done();	
-			// if (!user){
-			// 	return done(err);
-            // }
-            // assert(true);
-            // console.log(store);
-            // console.log(user);
-            // console.log(employee);
+            
+            /*
+            let availability = null; //Finding employee
+			await Availability.findOne({_id:employee.hours}, (error,a) => {
+                availability = a;
+            })
+            */
+            
+            USER = user;
+            STORE = store;
+            EMPLOYEE = employee;
+            //AVAILABILITY = availability;
         })
+        
     })
+    
 })
+
+
