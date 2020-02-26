3e157ba70716f705dd4cd02a22f6779a76ef5fcc
moose <mr.rammo@gmail.com>
2020-02-25 21:38:32 -0500

    allow JWT_AUTH header

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(function (req, res, next) {
 		res.setHeader('Access-Control-Allow-Origin', origin);
 	}
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,JWT_AUTH');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
